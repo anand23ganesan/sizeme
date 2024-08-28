234f514f883c20fcd8ba4afc23f854085747dab2
anand23ganesan <anand23ganesan@gmail.com>
2024-08-29 05:06:06 +0530

    sizeme original bata code in the server - standalone - initial commit

---
+++ assets/js/bata.js
@@ -0,0 +1,50 @@
+$(document).ready(function(){
+
+	$(".selected-form-age").on('change',function(){
+		var value = $(this).val();
+		$(".request-form-age").val(value);
+	});
+
+	$(".selected-shoesize").on('click',function(){
+		var value = $(this).val();
+		$(".request-systemsize").val(value);
+	});
+
+	$(".go-back-second").on('click',function(){
+		$("#fms-user-ins-page2").hide();
+		$("#fms-user-ins-page1").show();
+		$("#fms-user-ins-page3").hide();
+	});
+	$(".go-back-third").on('click',function(){
+		$("#fms-user-ins-page2").show();
+		$("#fms-user-ins-page1").hide();
+		$("#fms-user-ins-page3").hide();
+	});
+
+	$(".go-back-second-left").on('click',function(){
+		$("#fms-user-ins-left-page2").hide();
+		$("#fms-user-ins-left-page1").show();
+		$("#fms-user-ins-left-page3").hide();
+	});
+	$(".go-back-third-left").on('click',function(){
+		$("#fms-user-ins-left-page2").show();
+		$("#fms-user-ins-left-page1").hide();
+		$("#fms-user-ins-left-page3").hide();
+	});
+
+	$(".bata-close-application").on('click',function(){
+		 window.location.href = 'https://www.bata.in/';
+	})
+
+	$(".selected-shoesize-confirm").on('click',function(){
+		var value = $(this).val();
+		$(".custom-system-size").val(value);
+
+	})
+
+	$(".gender-select").on('click',function(){
+		var value = $(this).val();
+		$(".custom-gender-input").val(value);
+	});
+
+});
---
+++ desktop.php
@@ -0,0 +1,150 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/all.min.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+
+    <title>Bata</title>
+    <style type="text/css">
+      .container{
+        max-width: 1400px;
+      }
+      .text-inside-image{
+        padding: 60px 7px 0px 39px;
+        font-size: 22px;
+      }
+      .thankyou-text{
+          text-align: center;
+          font-size: 28px;
+          text-transform: uppercase;
+          color: #ED1C24;
+      }
+      .upper-section{
+        height: 48%;
+        background-color: #ebebeb;
+        padding: 40px 0px 0px 0px;
+        margin-bottom: 3%;
+        border:1px solid #000;
+
+      }
+      .upper-section > div {
+          height: 49%;
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
+          margin: 1% 0;
+      }
+      .upper-section p{
+        font-weight: 600;
+        line-height: normal;
+      }
+      .red-info {
+        border-radius: 0px;
+        max-width: 100%;
+        height: 48%;
+        border:1px solid #000;
+
+      }
+      .bottom-section {
+          height: 31.33%;
+          display: flex;
+          text-align: center;
+          justify-content: center;
+          align-items: center;
+          margin: 1% 0 0;
+          border: solid 1px #959595;
+          position: absolute;
+          bottom: 0;
+          width: calc(100% - 30px);
+      }
+      .bottom-section span{
+            color: #ed1c24;
+            font-weight: 800;
+      }
+      .phone-icon{
+        margin-bottom: 5px;
+      }
+      .bottom-section p{
+        color: #747070;
+        font-weight: 800;
+        margin-bottom: 0;
+        line-height: normal;
+      }
+      .home-image-style{
+        max-width: 85%;
+      }
+      .grey-area-cust-class {
+        margin-bottom:10px;
+        padding: 50px 15px;
+      }
+      .grey-area-cust-class p {
+        font-size:20px;
+        line-height: normal;
+      }
+      .bottom-section-cust-class {
+        padding: 20px 10px;
+      }
+      @media only screen and (max-width: 767px) {
+          .home-img-area {
+            margin-bottom: 10px;
+          }
+          .bottom-section {
+            bottom: unset;
+            margin-top: 15px;
+        }
+        .upper-section > div {
+              height: 50%;
+              margin: 5px 0;
+          }
+      }
+          
+    </style>
+  </head>
+  <body>
+    <section class="vertical-center">
+        <div class="container">
+           <div class="row" >
+              <div class="col-md-6 col-lg-6 col-xs-12 col-sm-12">
+                <div class="home-img-area">
+                    <h4 class="text-inside-image">We know that your shoe size is different for different shoe types and styles.</h4>
+                    <img src="assets/images/home-shoe.jpg" alt="shoe" class="img-fluid home-image-style">
+                </div>
+              </div>
+              <div class="col-md-6 col-lg-6 col-xs-12 col-sm-12" >
+                <div class="upper-section" style="margin-top:0px !important;">
+                  
+                  <div class="gray-area grey-area-cust-class" style="">
+                         <p style="">Now, measure your foot size virtually for every shoe type and get personalized recommendations.</p>
+                     </div>
+                </div>
+                <div class="red-info" style="padding: 68px 18px 0px 18px;margin-top: 0px !important;">
+                        <!-- <h5 class="thankyou-text" style="color:#fff;">Thank you</h5> -->
+                        <p style="color: #000;">Thank you for your interest in Bata Shoe Size Finder Application. However, this is a mobile only service. To proceed, just give a missed call on 75 07 77 02 13 and receive a secure link to find you shoe size in 3 easy steps.</p>
+                </div>
+                <!--  <div class="bottom-section bottom-section-cust-class" style="">
+                    <p>To proceed, just give a
+                    <br><span>missed call on <img src="assets/images/phoneicon.png" class="img-fluid phone-icon"> 75 07 77 02 13</span>
+                    <br>and receive a secure link to find your shoe size in 3 easy steps.</p>
+                </div> -->
+              </div>
+           </div>
+        </div>
+    </section>
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="assets/js/jquery-3.5.1.slim.min.js"></script>
+    <script src="assets/js/popper.min.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+
+
+  </body>
+</html>
